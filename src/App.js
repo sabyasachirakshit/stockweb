2240b92790b4a60e5fe110b59be20d3cd9ce3df8
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-10-15 18:10:19 +0530

    SETTING UP PATHS

--- src/App.js
+++ src/App.js
@@ -9,8 +9,26 @@ function App() {
       <Navbar />
       <Router>
         <Routes>
+        <Route path="/TORNTPOWER" element={<Instruments />} />
           <Route path="/TECHM" element={<Instruments />} />
           <Route path="/IDBI" element={<Instruments />} />
+          <Route path="/GRASIM" element={<Instruments />} />
+          <Route path="/BHARATFIN" element={<Instruments />} />
+          <Route path="/OFSS" element={<Instruments />} />
+          <Route path="/KAJARIACER" element={<Instruments />} />
+          <Route path="/INDIGO" element={<Instruments />} />
+          <Route path="/ENGINERSIN" element={<Instruments />} />
+          <Route path="/SBIN" element={<Instruments />} />
+          <Route path="/LALPATHLAB" element={<Instruments />} />
+          <Route path="/AARTIIND" element={<Instruments />} />
+          <Route path="/IPCALAB" element={<Instruments />} />
+          <Route path="/INDUSTOWER" element={<Instruments />} />
+          <Route path="/BIOCON" element={<Instruments />} />
+          <Route path="/ADANIPOWER" element={<Instruments />} />
+          <Route path="/CENTURYTEX" element={<Instruments />} />
+          <Route path="/DIVISLAB" element={<Instruments />} />
+          <Route path="/INFIBEAM" element={<Instruments />} />
+          <Route path="/HAVELLS" element={<Instruments />} />
           <Route path="/" element={<Table />} />
         </Routes>
       </Router>
